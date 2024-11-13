a917e7fd4533227eb940200fd0985cc7c0fd22d8
haeun1107 <haeun9394@gmail.com>
2024-11-13 20:01:17 +0900

    [Feature] Base code 작성

---
+++ src/main/java/dgu/farmer/global/base/BaseCode.java
@@ -0,0 +1,8 @@
+package dgu.farmer.global.base;
+
+public interface BaseCode {
+
+    ReasonDTO getReason();
+
+    ReasonDTO getReasonHttpStatus();
+}
---
+++ src/main/java/dgu/farmer/global/base/BaseErrorCode.java
@@ -0,0 +1,8 @@
+package dgu.farmer.global.base;
+
+public interface BaseErrorCode {
+
+    ErrorReasonDTO getReason();
+
+    ErrorReasonDTO getReasonHttpStatus();
+}
