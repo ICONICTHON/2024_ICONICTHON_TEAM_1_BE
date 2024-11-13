886112169b22ecfac1f12f19a89c00505715996b
haeun1107 <haeun9394@gmail.com>
2024-11-13 19:51:25 +0900

    [Init] 프로젝트 기본 세팅

---
+++ src/test/java/dgu/farmer/FarmerApplicationTests.java
@@ -0,0 +1,13 @@
+package dgu.farmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
