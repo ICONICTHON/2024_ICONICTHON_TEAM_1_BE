d704cf742406e204fabab9e481699c870cc9deb0
haeun1107 <haeun9394@gmail.com>
2024-11-14 17:02:58 +0900

    [Refactor] Request body DTO화

--- src/main/java/dgu/farmer/domain/s3/FileController.java
+++ src/main/java/dgu/farmer/domain/s3/FileController.java
@@ -13,8 +13,8 @@ public class FileController {
     private final FileService fileService;
 
     @PostMapping("/{prefix}/presigned-url")
-    public ResponseEntity<String> getPresignedUrl(@PathVariable String prefix, @RequestBody Map<String, String> requestBody) {
-        String fileName = requestBody.get("imageName");
+    public ResponseEntity<String> getPresignedUrl(@PathVariable String prefix, @RequestBody FileDTO fileDTO) {
+        String fileName = fileDTO.getImageName();
         String presignedUrl = fileService.getPreSignedUrl(prefix, fileName);
         return ResponseEntity.ok(presignedUrl);
     }
---
+++ src/main/java/dgu/farmer/domain/s3/FileDTO.java
@@ -0,0 +1,8 @@
+package dgu.farmer.domain.s3;
+
+import lombok.Data;
+
+@Data
+public class FileDTO {
+    private String imageName;
+}
